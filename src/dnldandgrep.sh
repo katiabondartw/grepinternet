109b301699a656abc2b6d814989b388c7f7bc266
Katia Bondar <kbondar@thoughtworks.com>
2018-04-30 15:54:06 +1000

    Improve download script, split input into 8 files

--- src/dnldandgrep.sh
+++ src/dnldandgrep.sh
@@ -3,7 +3,7 @@ PATHS_FILE="../data/wet.paths2"
 date
 COUNTER=1
 while read p; do
-  echo "s3://commoncrawl/$p" 
+  echo "$COUNTER s3://commoncrawl/$p" 
   aws s3 --no-sign-request cp "s3://commoncrawl/$p" - | gunzip -c | grep -o -f ../patterns/pattern | wc -l > "../res/$COUNTER"
   ((COUNTER=COUNTER+1))
 done < $PATHS_FILE
