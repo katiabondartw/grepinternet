8b2630170ee54effa6e3f9de53f5ee37ea39406c
Katia Bondar <kbondar@thoughtworks.com>
2018-05-03 09:57:12 +1000

    Fix awsinstance.sh script

--- src/awsinstance.sh
+++ src/awsinstance.sh
@@ -3,6 +3,7 @@ sudo yum install -y git
 cd /home/ec2-user
 git clone https://github.com/katiabondartw/grepinternet.git
 
+cd grepinternet/
 mkdir res
-cd grepinternet/src/
+cd src/
 ./dnldandgrep.sh $1
